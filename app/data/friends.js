aab91c1b59b65872f7be5f916d450276dbc1290f
Joe King <quickyster@yahoo.com>
2020-07-11 08:59:06 -0700

    added some basic code to a few files including potential matches

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the potential matches.
+// ===============================================================================
+
+var matchArray = [
+    {
+        matchName: "Jaq",
+        matchPhoto: "https://i.pinimg.com/originals/a8/86/27/a8862704596d572f3be54bfa1ef55456.jpg",
+        matchScores: 
+            [
+                
+            ]
+    },
+    {
+        matchName: "Gus",
+        matchPhoto: "https://i.pinimg.com/originals/8c/1e/bd/8c1ebd31c2204256f1a318942d7f1c07.jpg",
+        matchScores: 
+            [
+                
+            ]
+    },
+    {
+        matchName: "Mary",
+        matchPhoto: "https://vignette.wikia.nocookie.net/parody/images/4/46/Mary_in_Cinderella_II-_Dreams_Come_True.jpg/revision/latest?cb=20170601223110",
+        matchScores: 
+            [
+                
+            ]
+    },
+    {
+        matchName: "Mickey Mouse",
+        matchPhoto: "https://vignette.wikia.nocookie.net/disney/images/9/99/Mickey_Mouse_Disney_3.jpeg/revision/latest/top-crop/width/360/height/360?cb=20190305233033",
+        matchScores: 
+            [
+                
+            ]
+    },
+    {
+        matchName: "Minnie Mouse",
+        matchPhoto: "https://vignette.wikia.nocookie.net/disney/images/3/36/Minnie_Mouse_pose_.jpg/revision/latest/top-crop/width/360/height/450?cb=20170709133603",
+        matchScores: 
+            [
+                
+            ]
+    },
+    {
+        matchName: "Remi",
+        matchPhoto: "https://www.khwiki.com/images/thumb/7/77/Little_Chef_KHIII.png/250px-Little_Chef_KHIII.png",
+        matchScores: 
+            [
+                
+            ]
+    },
+    {
+        matchName: "Jerry",
+        matchPhoto: "https://upload.wikimedia.org/wikipedia/en/2/2f/Jerry_Mouse.png",
+        matchScores: 
+            [
+                
+            ]
+    },
+    {
+        matchName: "Miss Bianca",
+        matchPhoto: "https://i.pinimg.com/originals/3f/bb/b8/3fbbb8bafe3485d18520fe077eba674f.jpg",
+        matchScores: 
+            [
+                
+            ]
+    },
+    {
+        matchName: "Bernard",
+        matchPhoto: "https://vignette.wikia.nocookie.net/poohadventures/images/2/2f/Rescuers-down-under-disneyscreencaps.com-4389.jpg/revision/latest/scale-to-width-down/340?cb=20170313010051",
+        matchScores: 
+            [
+                
+            ]
+    },
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = matchArray;
+  
